f405de354690ea268842d4d3e7eef55da80e20ad
René Romero <rene.romero.2517@gmail.com>
2024-06-08 12:22:39 +0200

    Prepared exercise section 3-3

---
+++ Ch3_Ex3-3.py
@@ -0,0 +1,16 @@
+# 1. Write a function that draws a grid like the following:
+
+# +----+----+
+# |    |    |
+# |    |    |
+# |    |    |
+# |    |    |
+# +----+----+
+# |    |    |
+# |    |    |
+# |    |    |
+# |    |    |
+# +----+----+
+
+
+# 2. Write a function that draws a similar grid with four rows and four columns.
