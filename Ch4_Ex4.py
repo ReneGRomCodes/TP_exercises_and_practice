ddf921892c94feb6010699e0520298057a48e999
René Romero <rene.romero.2517@gmail.com>
2024-06-10 16:40:31 +0200

    Prepared assignments in 'Ch4_Ex4.py'

---
+++ Ch4_Ex4.py
@@ -0,0 +1,18 @@
+# Imports for this set of exercises.
+import turtle
+import math
+
+
+# 4-2: Write an appropriately general set of functions that can draw flowers as in Figure 4-1 (refer to book).
+
+
+# 4-3: Write an appropriately general set of functions that can draw figures as in Figure 4-2 (refer to book).
+
+
+# 4-4: The letters of the alphabet can be constructed from a moderate number of basic elements, like vertical and
+# horizontal lines and a few curves. Design an alphabet that can be drawn with a minimal number of basic elements and
+# then write functions that draw the letters.
+# You should write one function for each letter with names 'draw_a', 'draw_b', etc. and put your functions.
+
+
+# 4-5: Read about spirals at http://en.wikipedia.org/wiki/Spiral; then write a program draws an Archimedian spiral.
