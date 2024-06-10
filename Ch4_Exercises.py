361c4cf31121cd89ca11b00fb03eaf0add1e05c9
René Romero <rene.romero.2517@gmail.com>
2024-06-10 12:28:43 +0200

    Prepared 'Ch4_Exercises.py' with exercise assignments for turtle module.

---
+++ Ch4_Exercises.py
@@ -0,0 +1,22 @@
+# 1. Write a function called 'square' that takes a parameter named 't', which is a turtle. It should use the turtle to
+# draw a square.
+# Write a function that passes 'bob' as an argument to 'square' and then run the program again.
+
+
+# 2. Add another parameter, named 'length' to square. Modify the body so length of the sides is 'length' and then modify
+# the function call to provide a second argument. Run the program again. Test your program with a range of values for
+# length.
+
+
+# 3. Make a copy of square and change the name to 'polygon'. Add another parameter named 'n' and modify the body, so it
+# draws an n_sided regular polygon.
+
+
+# 4. Write a function called 'circle' that takes a turtle 't' and radius 'r' as parameters and draw an approximate
+# circle by calling 'polygon' with an appropriate length and number of sides. Test your function with a range of values
+# for 'r'.
+
+
+# 5. Make a more general version of 'circle' called 'arc' that takes an additional parameter 'angle', which determines
+# what fraction of a circle to draw. 'angle' is in units of degrees, so when 'angle=360', 'arc' should draw a complete
+# circle.
