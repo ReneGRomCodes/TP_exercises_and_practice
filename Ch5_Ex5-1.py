d1a9d9f217d10bd71c719060e76b29d62802fabe
René Romero <rene.romero.2517@gmail.com>
2024-06-11 17:02:38 +0200

    Added solution to 'Ch5_Ex5-1.py'

--- Ch5_Ex5-1.py
+++ Ch5_Ex5-1.py
@@ -6,3 +6,17 @@ print(time.time())
 
 # Write a script that reads the current time and converts it to a time of day in hours, minutes and seconds, plus the
 # number of days since the epoch.
+
+epoch = time.time()
+
+seconds_per_day = 86400
+seconds_per_hour = 3600
+seconds_per_minute = 60
+seconds_today = int(epoch % seconds_per_day)
+
+hour = seconds_today // seconds_per_hour
+minute = (seconds_today % seconds_per_hour) // seconds_per_minute
+seconds = seconds_today - (hour * seconds_per_hour) - (minute * seconds_per_minute)
+days_since_epoch = int(epoch // seconds_per_day)
+
+print(f"It's {hour:02d}:{minute:02d}:{seconds:02d} on day {days_since_epoch} of the epoch.")
