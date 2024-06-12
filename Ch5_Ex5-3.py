fc4fafb6de432ca807895b5317dc490d9bb6531c
René Romero <rene.romero.2517@gmail.com>
2024-06-12 11:10:20 +0200

    Added expanded solution to 'Ch5_Ex5-3.py'

--- Ch5_Ex5-3.py
+++ Ch5_Ex5-3.py
@@ -23,3 +23,42 @@ def is_triangle_input():
     c = int(input("Enter length of stick 'c': "))
 
     is_triangle(a, b, c)
+
+
+# Expanded solution that uses modified versions of the previous functions and turtle to draw a triangle.
+import math, turtle
+
+
+def is_triangle_2(a, b, c):
+    """Check if a triangle can be formed with the given length for 'a', 'b' and 'c'."""
+    if a + b > c and b + c > a and a + c > b:
+        return True
+    else:
+        return False
+
+
+def draw_triangle(t):
+    """Take user input for values 'a', 'b' and 'c', call 'is_triangle_2()' with these parameters. If return value is
+    'True', draw a triangle. 't' is a turtle."""
+    a = int(input("Enter length of stick 'a': "))
+    b = int(input("Enter length of stick 'b': "))
+    c = int(input("Enter length of stick 'c': "))
+
+    if is_triangle_2(a, b, c):
+        angle_a = 180 - math.degrees(math.acos((b**2 + c**2 - a**2) / (2 * b * c)))
+        angle_b = 180 - math.degrees(math.acos((a**2 + c**2 - b**2) / (2 * a * c)))
+        angle_c = 180 - math.degrees(math.acos((a**2 + b**2 - c**2) / (2 * a * b)))
+        t.fd(a)
+        t.lt(angle_c)
+        t.fd(b)
+        t.lt(angle_a)
+        t.fd(c)
+        t.lt(angle_b)  # Not really necessary. Only used to point the turtle back into it's starting orientation.
+    else:
+        print("That doesn't work.")
+
+
+triangle = turtle.Turtle()
+draw_triangle(triangle)
+
+turtle.mainloop()
