d997bf37dd6096c0991522787d11d8d97924924d
René Romero <rene.romero.2517@gmail.com>
2024-06-13 11:36:29 +0200

    Prepared exercises in 'Ch6_Ex6-4.py' and 'Ch6_Ex6-5.py'

---
+++ Ch6_Ex6-5.py
@@ -0,0 +1,4 @@
+# The greatest common divisor (GCD) of 'a' and 'b' is the largest number that divides both of them with no remainder.
+# One way to find the GCD of two numbers is base on the observation that ir 'r' is the remainder when 'a' is divided by
+# 'b', then 'gcd(a,b) = gcd(b,r)'. As a base case, we can use 'gcd(a,0) = a'.
+# Write a function called 'gcd' that takes parameters 'a' and 'b' and returns their GCD.
