83de16a3ad650d49bcdfe67fee91f16312ae12fc
René Romero <rene.romero.2517@gmail.com>
2024-06-13 13:06:09 +0200

    Added Solutions to exercises in 'Ch6_Ex6-4.py' and 'Ch6_Ex6-5.py'

--- Ch6_Ex6-4.py
+++ Ch6_Ex6-4.py
@@ -1,3 +1,18 @@
 # A number 'a' is a power of 'b' if it is divisible by 'b' and 'a / b' is a power of 'b'. Write a function called
-# 'is_power' that takes parameters 'a' and 'b' and returns 'True' if 'A' is a power of 'b'. Note: you will have to think
+# 'is_power' that takes parameters 'a' and 'b' and returns 'True' if 'a' is a power of 'b'. Note: you will have to think
 # about the base case.
+
+def is_power(a, b):
+    """Return 'True' if 'a' is a power of 'b'."""
+    if a/b == 1:
+        return True
+    elif a % b == 0:
+        return is_power(a/b, b)
+    else:
+        return False
+
+
+print(is_power(81, 3))  # True
+print(is_power(16, 2))  # True
+print(is_power(20, 3))  # False
+print(is_power(18, 2))  # False
--- Ch6_Ex6-5.py
+++ Ch6_Ex6-5.py
@@ -1,4 +1,17 @@
 # The greatest common divisor (GCD) of 'a' and 'b' is the largest number that divides both of them with no remainder.
-# One way to find the GCD of two numbers is base on the observation that ir 'r' is the remainder when 'a' is divided by
+# One way to find the GCD of two numbers is base on the observation that if 'r' is the remainder when 'a' is divided by
 # 'b', then 'gcd(a,b) = gcd(b,r)'. As a base case, we can use 'gcd(a,0) = a'.
 # Write a function called 'gcd' that takes parameters 'a' and 'b' and returns their GCD.
+
+def gcd(a, b):
+    """Return greatest common divisor of 'a' and 'b'."""
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a % b)
+
+
+print(gcd(48, 18))  # GCD is 6
+print(gcd(81, 27))  # GCD is 27
+print(gcd(56, 42))  # GCD is 14
+print(gcd(105, 35))  # GCD is 35
