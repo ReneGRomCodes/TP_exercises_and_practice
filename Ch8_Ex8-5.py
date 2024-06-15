7ddb72480e267f35d0593ad4c2c6f9c2c37faec1
René Romero <rene.romero.2517@gmail.com>
2024-06-15 11:28:27 +0200

    Prepared exercise in 'Ch8_Ex8-5.py'

---
+++ Ch8_Ex8-5.py
@@ -0,0 +1,8 @@
+# A Caesar cypher is a weak form of encryption that involves "rotating" each letter by a fixed number of places.
+# Write a function called 'rotate_word' that takes a string and an integer as parameters and returns a new string that
+# contains the letters from the original string rotated by the given amount.
+# You might want to use the built-in function 'ord', which converts a character to a numeric code, and 'chr' which
+# converts numeric codes to characters. But beware: numeric code for uppercase letters are different.
+
+def rotate_word(s, n):
+    pass
