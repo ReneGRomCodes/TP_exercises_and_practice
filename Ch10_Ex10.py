4f4086960c53cabe3caabd105a5ca58426cb72ef
René Romero <rene.romero.2517@gmail.com>
2024-06-22 12:17:20 +0200

    Prepared exercises 10-1 to 10-7 in 'Ch10_Ex10.py'

---
+++ Ch10_Ex10.py
@@ -0,0 +1,70 @@
+# Exercise 10-1: Write a function called 'nested_sum' that takes a list of lists of integers and adds up the elements
+# from all the nested lists.
+
+# For example:
+# t = [[1, 2], [3], [4, 5, 6]]
+# nested_sum(t)
+# 21
+
+
+
+
+
+# Exercise 10-2: Write a function called 'cumsum' that takes a list of numver and returns the cumulative sum; that is,
+# a new list where ith element is the sum of first i+1 elements from the original list.
+
+# For example:
+# t = [1, 2, 3]
+# cumsum(t)
+# [1, 3, 6]
+
+
+
+
+
+# Exercise 10-3: Write a function called 'middle' that takes a list and returns a new list that contains all but the
+# first and last elements.
+
+# For example:
+# t = [1, 2, 3, 4]
+# middle(t)
+# [2, 3]
+
+
+
+
+
+# Exercise 10-4: Write a function called 'chop' that takes a list, modifies it by removing the first and last elements,
+# and returns 'None'.
+
+# For example:
+# t = [1, 2, 3, 4]
+# chop(t)
+# print(t)
+# [2, 3]
+
+
+
+
+
+# Exercise 10-5: Write a function called 'is_sorted' that takes a list as a parameter and return 'True' if the list is
+# sorted in ascending order and 'False' otherwise.
+
+# For example:
+# print(is_sorted([1, 2, 2]))
+# True
+# print(is_sorted(['b', 'a']))
+# False
+
+
+
+
+
+# Exercise 10-6: Write a function called 'is_anagram' that takes two strings and returns 'True' if they are anagrams.
+
+
+
+
+
+# Exercise 10-7: Write a function called 'has_duplicates' that takes a list and returns 'True' if there is any element
+# that appears more than once. It should not modify the original list.
