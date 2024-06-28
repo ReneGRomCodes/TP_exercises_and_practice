518034f39e6b0b4ac80f5b6db1b1a621cfaac395
René Romero <rene.romero.2517@gmail.com>
2024-06-28 10:45:05 +0200

    Prepared exercises 11-1 to 11-5 in 'Ch11_Ex11.py'

---
+++ Ch11_Ex11.py
@@ -0,0 +1,20 @@
+# Exercise 11-1: Write a function that reads words in 'Ch9_words.txt' and stores them as keys in a dictionary. It
+# doesn't matter what the values are. Then you use the in operator as a fast way to check whether a string is in the
+# dictionary.
+
+
+# Exercise 11-2: Read the documentation of the dictionary method 'setdefault' and use it to write a more concise version
+# of 'invert_dict'.
+
+
+# Exercise 11-3: Memoize the Ackermann function from exercise 6-2 and see if memoization makes it possible to evaluate
+# the function with bigger arguments.
+
+
+# Exercise 11-4: If you did exercise 10-7, you already have a function named 'has_duplicates' that takes a list as a
+# parameter and returns 'True' if there is any object that appears more than once in the list. Use a dictionary to write
+# a faster, simpler version of 'has_duplicates'.
+
+
+# Exercise 11-5: Two words are "rotate pairs" if you can rotate one of them and get the other. Write a program that
+# reads a wordlist and finds all the rotate pairs.
