8e20fcdde7bb7327fe762ff972c066b42ed4013e
René Romero <rene.romero.2517@gmail.com>
2024-06-29 10:54:17 +0200

    Added solution to exercise 11-3 and 11-4 'Ch11_Ex11.py'

--- Ch11_Ex11.py
+++ Ch11_Ex11.py
@@ -5,20 +5,20 @@
 words = "Ch9_words.txt"
 
 
-def build_words_dict(t):
-    """Build a dictionary from txt file 't', return the lines as keys and the length of the lines as values."""
+def build_words_dict(file):
+    """Build a dictionary from txt file 'file', return the lines as keys and the length of the lines as values."""
     words_dict = dict()
 
-    with open(t) as f:
+    with open(file) as f:
         for line in f:
             words_dict[line[:-1]] = len(line) - 1  # [:-1] and -1 to remove the '\n' at the end of each line in file.
 
     return words_dict
 
 
-def find_word(s, t):
-    """Search for string 's' (case-insensitive) in dictionary build from txt file 't' and output message."""
-    search_words_dict = build_words_dict(t)
+def find_word(s, file):
+    """Search for string 's' (case-insensitive) in dictionary build from txt file 'file' and output message."""
+    search_words_dict = build_words_dict(file)
 
     if s.lower() in search_words_dict:
         print(f"The word '{s.upper()}' is in the dictionary with the assigned value {search_words_dict[s]}.")
@@ -41,7 +41,7 @@ def invert_dict(d):
 
 
 # Exercise 11-3: Memoize the Ackermann function from exercise 6-2 and see if memoization makes it possible to evaluate
-# the function with bigger arguments.
+# the function with bigger arguments than 'm=3, n=4'.
 
 # Function copied from exercise 6-2:
 def ack(m, n):
@@ -53,26 +53,37 @@ def ack(m, n):
         return ack(m-1, ack(m, n-1))
 
 
+known = {}
+
+
+def ack_memo(m, n):
+    if m in known:
+        return known[m]
+    if n in known:
+        return known[m]
+
+    if m == 0:
+        return n + 1
+    elif m > 0 and n == 0:
+        return ack(m-1, 1)
+    elif m > 0 and n > 0:
+        return ack(m-1, ack(m, n-1))
+
+
+
 # Exercise 11-4: If you did exercise 10-7, you already have a function named 'has_duplicates' that takes a list as a
 # parameter and returns 'True' if there is any object that appears more than once in the list. Use a dictionary to write
 # a faster, simpler version of 'has_duplicates'.
 
-def has_duplicates(t):
-    """Take a list 't' and return 'True' if any element appear more than once."""
-    t2 = sorted(t)
-    index = 0
+def has_duplicates_dict(t):
+    """Take list 't' and check for duplicate items (case-sensitive). Return 'True' if multiple items are found, 'False'
+    if not."""
+    t_dict = dict()
 
-    for i in range(len(t2)):
-        if t2[index] == t2[index-1]:
+    for item in t:
+        if item in t_dict:
             return True
         else:
-            index += 1
+            t_dict[item] = 1
 
     return False
-
-
-# Exercise 11-5: Two words are "rotate pairs" if you can rotate one of them and get the other. Write a program that
-# reads a wordlist and finds all the rotate pairs.
-
-def find_rotate_pairs(file):
-    pass
