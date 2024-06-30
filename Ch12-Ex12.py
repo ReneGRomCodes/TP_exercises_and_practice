c497cf981619443defc24e455a83c5a35696601c
René Romero <rene.romero.2517@gmail.com>
2024-06-30 12:03:52 +0200

    Prepared exercises 12-1, 12-2 and 12-3 in 'Ch12_Ex12.py'

---
+++ Ch12-Ex12.py
@@ -0,0 +1,21 @@
+# Exercise 12-1: Write a function called 'most_frequent' that takes a string and prints the letters in decreasing order
+# of frequency. Find text samples from several different languages and see how letter frequency varies between
+# languages.
+
+def most_frequent(s):
+    pass
+
+
+# Exercise 12-2: More anagrams!
+# 1. Write a program that reads a word list from a file and prints all the sets of words that are anagrams.
+# 2. Modify the previous program so that it prints the longest list of anagrams first, followed by the second longest,
+#    and so on.
+# 3. In Scrabble, a "bingo" is when you play all seven tiles in your rack, along with a letter on the board, to form an
+#    eight-letter word. What collection of eight letters forms the most possible bingos'
+
+
+# Exercise 12-3: Two words form a "metathesis pair" if you can transform one into the other by swapping two letters; for
+# example, "converse" and "conserve". Write a program that finds all the metathesis pairs in the dictionary.
+
+def find_metathesis_pair(file):
+    pass
