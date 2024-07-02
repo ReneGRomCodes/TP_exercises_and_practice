ca268846d2ac94b8629aae9416e41dd013ad9585
René Romero <rene.romero.2517@gmail.com>
2024-07-02 16:45:09 +0200

    Prepared exercise 12-4 in 'Ch12_Ex12.py' and made changes to 'Ch9_words.txt' for said exercise

--- Ch12-Ex12.py
+++ Ch12-Ex12.py
@@ -28,3 +28,15 @@ def most_frequent(text):
 
 def find_metathesis_pair(file):
     pass
+
+
+# Exercise 12-4: View the book for the full "Car Talk Puzzler".
+# Write a program to find all words that can be reduced this way and then the longest one.
+# Suggestions:
+# 1. You might want to write a function that takes a word and computes a list of all the words that can be formed by
+#    removing one letter. These are the "children" of the word.
+# 2. Recursively, a word is reducible if any of its children are reducible. As a base case, you can consider the empty
+#    string reducible.
+# 3. The wordlist I provided, "Ch9_words.txt", doesn't contain single letter words. So you might want to add "I", "a"
+#    and the empty string.
+# 4. To improve the performance of your program, you might want to memoize the words that are known to be reducible.
