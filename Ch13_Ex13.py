f6290686bbf46abeb22e4899bda2b67f89cb5112
René Romero <rene.romero.2517@gmail.com>
2024-07-06 10:25:55 +0200

    Prepared exercises 13-1 to 13-4 in 'Ch13_Ex13.py'

---
+++ Ch13_Ex13.py
@@ -0,0 +1,19 @@
+# Exercise 13-1: Write a program that reads a file, breaks each line into words, strips whitespace and punctuation from
+# the words and converts them to lowercase.
+
+
+# Exercise 13-2: Go to Project Gutenberg (http://gutenberg.org) and download your favorite out-of-copyright book in
+# plain text format.
+# Modify your program from the previous exercise to read the book you downloaded, skip over the header information at
+# the beginning of the file and process the rest of the words as before.
+# Then modify the program to count the total number of words in the book and the number of times each word is used.
+# Print the number of different words used in the book. Compare different books by different authors, written in
+# different eras. Which author uses the most extensive vocabulary?
+
+
+# Exercise 13-3: Modify the program from the previous exercise to print the 20 most frequently used words in the book.
+
+
+# Exercise 13-4: Modify the previous program to read a word list (use 'Ch9_words.txt') and then print all the words in
+# the book that are not in the word list. How many of them are typos? How many of them are common words that should be
+# in the word list and how many of them are really obscure?
