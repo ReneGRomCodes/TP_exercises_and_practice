9c334f082cf5fd85e18ab285cecfab965d84de89
ReneGRomCodes <renegromcodes@gmail.com>
2025-08-10 14:59:33 +0200

    added solution for exercise 14-2.

--- Chapter_14.py
+++ Chapter_14.py
@@ -7,10 +7,10 @@
 def sed(p_string, r_string, origin_file, target_file):
     """Replace occurrences of 'p_string' in 'origin_file' with 'r_string' and save new text in 'target_file'.
     ARGS:
-        p_string:
-        r_string:
-        origin_file:
-        target_file:
+        p_string: pattern string to be replaced with 'r_string'.
+        r_string: replacement string to replace occurrences of 'p_string'.
+        origin_file: path to the file to process.
+        target_file: path to the file to save processed text.
     """
     try:
         fin = open(origin_file, "r")
@@ -27,6 +27,13 @@ def sed(p_string, r_string, origin_file, target_file):
 
 
 def sed_alternative(p_string, r_string, origin_file, target_file):
+    """Alternative solution using 'with open' command and more detailed error handling.
+    ARGS:
+        p_string: pattern string to be replaced with 'r_string'.
+        r_string: replacement string to replace occurrences of 'p_string'.
+        origin_file: path to the file to process.
+        target_file: path to the file to save processed text.
+    """
     try:
         with open(origin_file, "r") as fin, open(target_file, "w") as fout:
             fin_contents = fin.read().replace(p_string, r_string)
@@ -39,12 +46,70 @@ def sed_alternative(p_string, r_string, origin_file, target_file):
     except OSError:
         print("Error! Exiting program.")
 
+
 # Exercise 14-2: Use the solution from exercise 12-2 to create a dictionary that maps from sorted string of letters to
 # a list of words containing anagrams.
 # Write a module that provides two new functions: 'store_anagrams()' to store the anagram dictionary in a "shelf", and
-# read anagrams to look up a words and return a list of its anagrams.
+# 'read_anagrams()' to look up a words and return a list of its anagrams.
 """NOTE: The book asks you to download the solution from 'http://thinkpython2.com/code/anagram_sets.py', but at the time
 of my writing this, the site seems to be down."""
+import shelve
+
+
+def sort_letters(s):
+    """Take string 's' and return new string in alphabetical order."""
+    return ''.join(sorted(list(s.lower())))
+
+
+def find_anagrams(file):
+    """Take text from 'file' and return dict of anagrams.
+    ARGS:
+        file: path to the file to process.
+    RETURNS:
+        anagrams_dict
+    """
+    anagrams_dict = dict()
+
+    with open(file, "r") as f:
+        text = f.read()
+        words_set = set(text.split())
+
+        for word in words_set:
+            sorted_word = sort_letters(word)
+            if sorted_word not in anagrams_dict:
+                anagrams_dict[sorted_word] = {word}
+            else:
+                anagrams_dict[sorted_word].add(word)
+
+    return anagrams_dict
+
+
+def store_anagram(text, file):
+    """Create and store anagrams dictionary from text into a shelf file.
+    Args:
+        text: path to the text file to process.
+        file: path for the shelf file to store anagrams.
+    """
+    anagrams_dict = find_anagrams(text)
+
+    with shelve.open(file) as shelf:
+        for k, v in anagrams_dict.items():
+            shelf[k] = v
+
+
+def read_anagrams(word, file):
+    """Retrieve a list of anagrams for a given word from a shelf file.
+    ARGS:
+        word: Word to find anagrams for.
+        file: Path to the shelf file to read from.
+    RETURNS:
+        list: List of anagrams for the word.
+    """
+    sorted_word = sort_letters(word)
+
+    with shelve.open(file, "r") as shelf:
+        return list(shelf[sorted_word])
+
 
 
 # Exercise 14-3: In a large collection of MP3 files, there may be more than one copy of the same song, stored in different
